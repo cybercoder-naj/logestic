2e87acf73e99b7a8184700e34c07b3efae3bd3f5
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-03-21 15:47:30 +0000

    refactor: separated out buildAttrs

--- src/index.ts
+++ src/index.ts
@@ -3,72 +3,15 @@
  *   It allows for customizable logging of HTTP requests and responses.
  */
 
-import Elysia, { type Context } from 'elysia';
-import { Attribute, AttributeMap, FormatObj, Presets } from './types';
+import Elysia from 'elysia';
+import { Attribute, FormatObj, Presets } from './types';
 import presets from './presets';
 import { BunFile } from 'bun';
 import c from 'chalk';
+import { buildAttrs } from './utils';
 
 export type { Attribute };
 export const chalk = c; // Re-export chalk for custom formatting
-
-/**
- * Builds an attribute object containing the requested attributes from the context.
- * @param ctx - The context of the current request.
- * @param reqAttrs - A map of requested attributes.
- * @returns An object containing the requested attributes.
- */
-const buildAttrs = (ctx: Context, reqAttrs: AttributeMap): Attribute => {
-  const { request, path, body, query, set } = ctx;
-
-  let attrs: Attribute = {};
-  for (const key in reqAttrs) {
-    const k = key as keyof Attribute;
-    switch (k) {
-      case 'ip':
-        attrs.ip = request.headers.get('x-forwarded-for') || '<ip?>';
-        break;
-
-      case 'method':
-        attrs.method = request.method;
-        break;
-
-      case 'path':
-        attrs.path = path;
-        break;
-
-      case 'body':
-        attrs.body = body;
-        break;
-
-      case 'query':
-        attrs.query = query;
-        break;
-
-      case 'time':
-        attrs.time = new Date();
-        break;
-
-      case 'contentLength':
-        attrs.contentLength = Number(request.headers.get('content-length'));
-        break;
-
-      case 'status':
-        attrs.status = set.status;
-        break;
-
-      case 'referer':
-        attrs.referer = request.headers.get('referer') || '<referer?>';
-        break;
-
-      case 'userAgent':
-        attrs.userAgent = request.headers.get('user-agent') || '<user-agent?>';
-        break;
-    }
-  }
-
-  return attrs;
-};
 
 /**
  * Logestic class provides methods to configure and perform logging.
